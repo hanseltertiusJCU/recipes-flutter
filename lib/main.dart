f8a9b4ebdfb330cbc5fc9cb30e7d8c7f9b3c0029
Hansel Tertius <hansel.tertius@my.jcu.edu.au>
2019-04-15 10:10:13 +0700

    Implement a list of items

--- lib/main.dart
+++ lib/main.dart
@@ -7,21 +7,43 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        title: 'Bahan Makanan',
+        // Implement basic material design visual layout structure;
+        // related to something to do in content layout
+        home: new Scaffold(
+          // app bar to display at the top of Scaffold
+          appBar: new AppBar(
+            // Set app bar title
+            title: const Text('Bahan Makanan'),
+          ),
+          // the content of the Scaffold
+          body: ListView(
+            children: <Widget>[
+              // represents the item of ListView
+              ListTile(
+                title: Text('Chicken'),
+              ),
+              ListTile(
+                title: Text('Salmon'),
+              ),
+              ListTile(
+                title: Text('Beef'),
+              ),
+              ListTile(
+                title: Text('Pork'),
+              ),
+              ListTile(
+                title: Text('Avocado'),
+              ),
+              ListTile(
+                title: Text('Apple Cider Vinegar'),
+              ),
+              ListTile(
+                title: Text('Asparagus'),
+              )
+            ],
+          ),
+        ));
   }
 }
 
